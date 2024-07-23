78fa271662e51573c8d181fad8e21e3fe4e7095d
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-07-23 21:45:34 +0100

    Fix User

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""
+""" 
 User class
 """
 
-
 class User():
     """ Documentation """
+
+    __email = None
 
     def __init__(self):
         """ Documentation """
         self.__email = None
 
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
+    
     @email.setter
     def email(self, value):
         """ Documentation """
@@ -18,12 +24,9 @@ class User():
             raise TypeError("email must be a string")
         self.__email = value
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
-
-
+    
+   
+    
 if __name__ == "__main__":
 
     u = User()
